173883b89a785c46b2ae807278a94ceaf9cf5a34
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2023-05-06 19:18:55 +0000

    Extend base template to register.html

--- sewingsquad/routes.py
+++ sewingsquad/routes.py
@@ -7,3 +7,8 @@ from sewingsquad.models import Users, SewingWorks
 @app.route("/")
 def landing_page():
     return render_template("base.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
