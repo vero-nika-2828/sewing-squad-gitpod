743b0ecfe9670086bda01d4e1057ba0a2d4d2a7c
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2023-05-06 18:58:08 +0000

    Add code to run app in run.py

---
+++ run.py
@@ -0,0 +1,11 @@
+# Imports
+import os
+from sewingsquad import app
+
+# Runs the application
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
+    )
