f910c81d349a6d9a5d28158a57781fde5ac4fdba
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2023-05-07 04:13:46 +0000

    Add login functionality

--- sewingsquad/routes.py
+++ sewingsquad/routes.py
@@ -1,5 +1,6 @@
 # Imports
 from flask import render_template, request, flash, session, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
 from sewingsquad import app, db
 from sewingsquad.models import Users, SewingWorks
 
@@ -13,11 +14,11 @@ def landing_page():
 def register():
     if request.method == "POST":
         # check if username already exists
-        registered_user = Users.query.filter(
+        existing_user = Users.query.filter(
             Users.username == request.form.get("username").lower()).all()
 
         # If username already exists - flash messages
-        if registered_user:
+        if existing_user:
             flash("This username already exists."), 
             flash("Please choose another unsername")
             return redirect(url_for("template.register"))
@@ -39,6 +40,31 @@ def register():
     return render_template("register.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    # check if username exists in database
+    if request.method == "POST":
+        existing_user = Users.query.filter(
+            Users.username == request.form.get("username").lower()).all()
+    
+        if existing_user:
+            # ensure hashed pasword matches use input
+            if check_password_hash(
+                    existing_user[0].password,
+                    request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome back,{}".format(
+                    request.form.get("firstname").capitalize()))
+                return redirect(
+                    url_for(
+                        "my_work",
+                        username=session["user"]))
+
+            else:
+                flash("Incorrect Username and/or Password") 
+                return redirect(url_for("login"))   
+
+        else:
+            flash("Incorrect Username and/or Password")       
+
     return render_template("login.html")
