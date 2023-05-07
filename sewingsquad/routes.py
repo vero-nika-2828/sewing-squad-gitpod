f3b59393dc497a4e4d6546089f2c9944ed20d429
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2023-05-07 03:22:02 +0000

    Add defensive programming for field input

--- sewingsquad/routes.py
+++ sewingsquad/routes.py
@@ -37,3 +37,8 @@ def register():
         return redirect(url_for("login"))
 
     return render_template("register.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
