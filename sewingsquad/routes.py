a3eebbd222fd3be8ee3c37b035633f3a53946033
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2023-05-07 02:35:35 +0000

    Add registration functionality in routes.py

--- sewingsquad/routes.py
+++ sewingsquad/routes.py
@@ -1,5 +1,5 @@
 # Imports
-from flask import render_template
+from flask import render_template, request, flash, session, redirect, url_for
 from sewingsquad import app, db
 from sewingsquad.models import Users, SewingWorks
 
@@ -11,4 +11,29 @@ def landing_page():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if username already exists
+        registered_user = Users.query.filter(
+            Users.username == request.form.get("username").lower()).all()
+
+        # If username already exists - flash messages
+        if registered_user:
+            flash("This username already exists."), 
+            flash("Please choose another unsername")
+            return redirect(url_for("template.register"))
+
+        newuser = Users(
+            firstname=request.form.get("firstname").lower(),
+            username=request.form.get("username").lower(),
+            email=request.form.get("email").lower(),
+            password=generate_password_hash(request.form.get("password")),
+        )
+
+        # Add to database
+        db.sessionadd(newuser)
+        db.session.commit()
+
+        flash("Registration successful")
+        return redirect(url_for("log_in"))
+
     return render_template("register.html")
