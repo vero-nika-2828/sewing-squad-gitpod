f7020a68f0a75ed227640463be91b5dcaa65b14e
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2023-06-27 20:54:18 +0000

    Add switch-images functionality in script.js

--- sewingsquad/static/js/script.js
+++ sewingsquad/static/js/script.js
@@ -4,7 +4,23 @@ $(document).ready(function () {
     $('select').formSelect();
     $('.modal').modal();
     $('.collapsible').collapsible();
+    $('.secondary-image').hide();
+    
 });
+
+$('.card-image').on("mouseenter", function(){ 
+    $(this).hide();
+    $(this).siblings('.secondary-image').show();
+    console.log("It works");
+}).on("mouseleave", function(){
+    var current_image = this
+
+    setTimeout(function(){
+        $(current_image).show();
+        $(current_image).siblings('.secondary-image').hide();
+    },500);
+});
+
 
 //Display the current year in footer's copyright
 $('#year').html(new Date().getFullYear());
