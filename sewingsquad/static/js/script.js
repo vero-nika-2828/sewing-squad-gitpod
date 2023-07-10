d420c14be34788f85a82aa323b77a0ff3aca076e
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2023-07-10 21:12:43 +0000

    Add real user testing in TESTING.md

--- sewingsquad/static/js/script.js
+++ sewingsquad/static/js/script.js
@@ -15,10 +15,10 @@ $(document).ready(function () {
 $('.primary-image').on("mouseenter", function(){ 
     $(this).hide();
     $(this).siblings('.secondary-image').show();
-})
+});
 
 $('.secondary-image').on("mouseleave", function(){
-    var current_image = this
+    var current_image = this;
         $(current_image).hide();
         $(current_image).siblings('.primary-image').show();
 
@@ -26,12 +26,12 @@ $('.secondary-image').on("mouseleave", function(){
 
 
 $('.next').on("click", function(){
-    $('.carousel.carousel-slider').carousel("next")
-})
+    $('.carousel.carousel-slider').carousel("next");
+});
 
 $('.prev').on("click", function(){
-    $('.carousel.carousel-slider').carousel("prev")
-})
+    $('.carousel.carousel-slider').carousel("prev");
+});
 
 //Display the current year in footer's copyright
 $('#year').html(new Date().getFullYear());
